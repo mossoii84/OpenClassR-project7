57db3d0cc04f3f9a696a1bd4e3c17d7654175cf4
mossoii84 <mossomkh@gmail.com>
2023-01-02 14:18:04 +0100

    collapsible flash

--- src/components/Collapsible.jsx
+++ src/components/Collapsible.jsx
@@ -1,7 +1,12 @@
 import { useState } from 'react';
 import './Collapsible.css'
-import  arrowUp from '../assets/arrow-up.webp'
-import  arrowDown from '../assets/arrow-down.webp'
+// import  arrowUp from '../assets/arrow-up.webp'
+// import  arrowDown from '../assets/arrow-down.webp'
+
+import {
+  TfiAngleUp,
+  TfiAngleDown
+} from "react-icons/tfi";
 
 
 export default function Collapsible(props){
@@ -13,8 +18,8 @@ return (
         <div className='collapsible'>
           <div  className="titleCollapsible">{props.label}</div>
             <div className='imgUpDown'>
-                 { open ? <img src={arrowDown} alt=''/>
-                       : <img src={arrowUp} alt=''/> } 
+                 { open ? <TfiAngleUp className='tfiIcon'/>
+                        : <TfiAngleDown className='tfiIcon'/> } 
             </div>
         </div>   
             {open && (
